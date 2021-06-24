7305fb1bdc5df5dd6a4ea01594ffba5f3f54b82d
sahadat <shmanik14@yahoo.com>
2021-06-24 14:20:16 +0600

    add category

--- src/component/Category/Category.js
+++ src/component/Category/Category.js
@@ -1,10 +1,11 @@
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import * as classes from './Category.module.css';
 
 const Category = () => {
     return (
-        <div className="blog-category">
+        <div className={classes.blogcategory}>
             <div className="container mx-auto grid grid-cols-4 gap-4">
                 <div className="sin-category text-center">
                     <p><span><FontAwesomeIcon icon={faUser} /></span></p>
